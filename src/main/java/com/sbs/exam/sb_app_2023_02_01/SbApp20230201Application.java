7a59cd1735d0030e918a8d0c801d268644c28321
eggfulllife <ghkdtjdrnek@naver.com>
2023-02-01 15:12:11 +0900

    세팅

---
+++ src/main/java/com/sbs/exam/sb_app_2023_02_01/SbApp20230201Application.java
@@ -0,0 +1,13 @@
+package com.sbs.exam.sb_app_2023_02_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApp20230201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApp20230201Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbs/exam/sb_app_2023_02_01/SbApp20230201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.exam.sb_app_2023_02_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApp20230201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
